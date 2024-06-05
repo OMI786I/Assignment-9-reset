a0b3fb4ce13653d305deaa2241f1e9d95d97288b
OMI786I <sohrawardy1998@gmail.com>
2024-06-05 22:12:33 +0600

    getting started with loading data

--- assignment-9/src/Components/Home.jsx
+++ assignment-9/src/Components/Home.jsx
@@ -1,6 +1,16 @@
+import { useEffect, useState } from "react";
 import Banner from "./Banner";
 
 const Home = () => {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    fetch("/data.json")
+      .then((res) => res.json())
+      .then((data) => setData(data));
+  }, []);
+
+  console.log(data);
+
   // Luxury:Penthouse, Beachfront properties, resorts, private islands,Villas,Mensions
   return (
     <div>
